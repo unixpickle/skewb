279fd1a51fdc6f396fd56989c19ebd912766ffa5
Alex Nichol <unixpickle@gmail.com>
2015-01-28 16:40:24 -0500

    started implementing WCA moves & piece cube

--- skewb.go
+++ skewb.go
@@ -1,82 +1,94 @@
 package skewb
 
+// A Corner is one of the eight corner pieces on the puzzle.
+//
+// A corner's orientation is determined by the direction the x sticker is
+// facing. 0 means it's normal to the x axis, 1 the y axis, and 2 the z axis.
+//
+// A corner's index is the binary triple XYZ with the origin at the bottom back
+// left corner of the cube.
+type Corner struct {
+	Piece       int8
+	Orientation int8
+}
+
 // A Skewb represents the state of a Skewb.
-type Skewb [30]int8
+type Skewb struct {
+	Centers [6]int8
+	Corners [8]Corner
+}
 
 // NewSkewb returns a solved skewb.
 func NewSkewb() *Skewb {
 	var res Skewb
-	for i := int8(0); i < 30; i++ {
-		res[i] = (i/5) + 1
+	
+	// Create corners
+	for i := uint8(0); i < 8; i++ {
+		res.Corners[i].Piece = i
 	}
+	
+	// Create centers
+	for i := uint8(0); i < 6; i++ {
+		res.Centers[i] = i
+	}
+	
 	return &res
 }
 
-// ClockDFL does a DFL move.
-func (s *Skewb) ClockDFL() {
-	s[12], s[7], s[27] = s[27], s[12], s[7] // check
-	s[14], s[8], s[26] = s[26], s[14], s[8] // check
-	s[23], s[19], s[3] = s[3], s[23], s[19] // check
-	s[6], s[28], s[10] = s[10], s[6], s[28] // check
-	s[13], s[5], s[29] = s[29], s[13], s[5] // check
+// TurnB performs a rotation of the B face which corresponds to the bottom back
+// left side of the Skewb.
+func (s *Skewb) TurnB(clock bool) {
+	// TODO: this
 }
 
-// ClockDFR does a DFR move.
-func (s *Skewb) ClockDFR() {
-	s[12], s[22], s[7] = s[7], s[12], s[22] // check
-	s[20], s[9], s[13] = s[13], s[20], s[9] // check
-	s[4], s[18], s[29] = s[29], s[4], s[18] // check
-	s[11], s[24], s[5] = s[5], s[11], s[24] // check
-	s[14], s[23], s[6] = s[6], s[14], s[23] // check
+// TurnL performs a rotation of the L face which corresponds to the bottom front
+// left corner of the Skewb.
+func (s *Skewb) TurnL(clock bool) {
+	// TODO: this
 }
 
-// ClockUFL does a UFL move.
-func (s *Skewb) ClockUFL() {
-	s[12], s[27], s[2] = s[2], s[12], s[27] // check
-	s[4], s[13], s[25] = s[25], s[4], s[13] // check
-	s[11], s[29], s[0] = s[0], s[11], s[29] // check
-	s[20], s[5], s[16] = s[16], s[20], s[5] // check
-	s[3], s[10], s[26] = s[26], s[3], s[10] // check
-}
-
-// ClockUFR does a UFR move.
-func (s *Skewb) ClockUFR() {
-	s[2], s[22], s[12] = s[12], s[2], s[22] // check
-	s[21], s[14], s[3] = s[3], s[21], s[14] // check
-	s[23], s[10], s[1] = s[1], s[23], s[10] // check
-	s[15], s[6], s[26] = s[26], s[15], s[6] // check
-	s[4], s[20], s[11] = s[11], s[4], s[20] // check
-}
-
-// Move performs a move.
-func (s *Skewb) Move(m Move) {
-	count := 1
-	if !m.Clock {
-		count = 2
-	}
-	for i := 0; i < count; i++ {
-		switch m.Side {
-		case 0:
-			s.ClockDFL()
-		case 1:
-			s.ClockDFR()
-		case 2:
-			s.ClockUFL()
-		case 3:
-			s.ClockUFR()
+// TurnR performs a rotation of the R face which corresponds to the bottom back
+// right corner of the Skewb.
+func (s *Skewb) TurnR(clock bool) {
+	// Permute centers and corners.
+	s.Centers[4], s.Centers[3], s.Centers[1] = s.Centers[1], s.Centers[4],
+		s.Centers[3]
+	s.Corners[3], s.Corners[0], s.Corners[5] = s.Corners[5], s.Corners[3],
+		s.Corners[0]
+	
+	// Change corner orientations.
+	for _, i := range []int{0, 1, 3, 5} {
+		if s.Corners[i].Orientation == 0 {
+			s.Corners[i].Orientation = 2
+		} else if s.Corners[i].Orientation == 2 {
+			s.Corners[i].Orientation = 1
+		} else {
+			s.Corners[i].Orientation = 0
 		}
 	}
 }
 
-// Solved returns true if the skewb is solved in any orientation.
+// TurnU performs a rotation of the F face which corresponds to the top back
+// left corner of the Skewb.
+func (s *Skewb) TurnU(clock bool) {
+	// TODO: this
+}
+
+// Solved returns true if the skewb is solved and oriented correctly.
 func (s *Skewb) Solved() bool {
-	for i := 0; i < 6; i++ {
-		color := s[i*5]
-		for j := i*5+1; j < (i+1)*5; j++ {
-			if s[j] != color {
-				return false
-			}
+	// Check corners.
+	for i := uint8(0); i < 8; i++ {
+		if res.Corners[i].Piece != i || res.Corners[i].Orientation != 0 {
+			return false
 		}
 	}
+	
+	// Check centers
+	for i := uint8(0); i < 6; i++ {
+		if res.Centers[i] != i {
+			return false
+		}
+	}
+	
 	return true
 }
