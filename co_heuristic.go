b8c4e15ce4fd347bed96b56bcf5ddc7cc0ea461b
Alex Nichol <unixpickle@gmail.com>
2015-01-29 18:00:42 -0500

    drafted COHeuristic

---
+++ co_heuristic.go
@@ -0,0 +1,54 @@
+package skewb
+
+// COHeuristic stores a move count for each corner configuration.
+type COHeuristic map[int]int
+
+// MakeCOHeuristic uses breadth-first search to generate a COHeuristic.
+func MakeCOHeuristic(moves []Move) COHeuristic {
+	res := COHeuristic{}
+	
+	// Create the starting nodes
+	nodes := make([]searchNode, 24)
+	for i, x := range NewSkewb().AllRotations() {
+		nodes[i] = searchNode{x, 0}
+	}
+	
+	// Do the search
+	for len(nodes) > 0 {
+		node := nodes[0]
+		nodes = nodes[1:]
+		idx := encodeCO(&node.state)
+		if _, ok := res[idx]; ok {
+			continue
+		}
+		res[idx] = node.depth
+		for _, move := range moves {
+			newState := node.state
+			newState.Move(move)
+			nodes = append(nodes, searchNode{newState, node.depth + 1})
+		}
+	}
+	
+	return res
+}
+
+// MinMoves returns the minimum number of moves needed to orient the corners in
+// some orientation.
+func (c COHeuristic) MinMoves(s *Skewb) int {
+	return c[encodeCO(s)]
+}
+
+func encodeCO(s *Skewb) int {
+	res := 0
+	mul := 1
+	for i := 0; i < 8; i++ {
+		res += int(s.Corners[i].Orientation) * mul
+		mul *= 3
+	}
+	return res
+}
+
+type searchNode struct {
+	state Skewb
+	depth int
+}
